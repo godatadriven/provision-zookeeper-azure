ba53bbf1e7204720fd42a10e71d717e03431fde6
Tunde Alkemade <tundealkemade@godatadriven.com>
2017-09-18 15:45:22 +0200

    remove sudo as script is executed as root by Azure

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm"
-sudo yum localinstall jdk-8u144-linux-x64.rpm
+yum localinstall jdk-8u144-linux-x64.rpm
 
 cd /usr/local
 
