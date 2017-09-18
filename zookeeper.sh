76769900a217d616d8d8d90231b09acb2186f04c
Tunde Alkemade <tundealkemade@godatadriven.com>
2017-09-18 16:05:20 +0200

    added path

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /tmp
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm"
 yum localinstall jdk-8u144-linux-x64.rpm
 
